ed8efbc823a1147f84136e4c64334fc49a9ba63c
PianoHagens <pianohagens@gmail.com>
2018-05-20 19:44:39 -0700

    a little change on date format
    Result: Today is Sunday, May 20th, 2018, 19:41 pm.

--- application/views/admins/index.php
+++ application/views/admins/index.php
@@ -7,10 +7,10 @@
 <?php 
 if ($first_name != ""){
     echo '<img src="../img/' . $picture . '" width="100" height="100"' . '>' . '<h4>' . $email . '</h4><br>'; 
-    echo '<h1>Welcome ' . $first_name . " " . $last_name . '!<br><hr><br>' . $logged . "</h1><br> Today is " . date('D, M d\t\h, Y, H:i a.');
+    echo '<h1>Welcome ' . $first_name . " " . $last_name . '!<br><hr><br>' . $logged . "</h1><br> Today is " . date('l, M jS, Y, H:i a.');
 }else{
     
-    echo '<h1>' . $logged . '</h1><br>' . 'It is ' . date('D, M d\t\h, Y, H:i a.');
+    echo '<h1>' . $logged . '</h1><br>' . 'It is ' . date('l, M jS, Y, H:i a.');
 }
 
 ?>
