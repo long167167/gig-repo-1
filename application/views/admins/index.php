ae8a2295c6e83b7a725073af7bda14666b7d346e
Piano Hagens <pianohagens@gmail.com>
2018-06-10 14:16:53 -0700

    Login Histories for Amind Dashboard 2
    I created a table to contain my login history view

--- application/views/admins/index.php
+++ application/views/admins/index.php
@@ -1,13 +1,35 @@
 <?php $this->load->view($this->config->item('theme') . 'header'); ?>
-<!--
-<h1>
-    //"Hi " . $first_name . ", "?>//$logged
-</h1>
--->
 <?php 
 if ($first_name != ""){
-    echo '<img src="../img/' . $picture . '" width="100" height="100"' . '>' . '<h4>' . $email . '</h4><br>'; 
-    echo '<h1>Welcome ' . $first_name . " " . $last_name . '!<br><hr><br>' . $logged . "</h1><br> Today is " . date('l, M jS, Y, H:i a.');
+    echo '<img src="../img/' . $picture . '" width="100" height="100"' . '>' . '<h4>' . $email . '</h4><br>';
+
+    echo '<h1>Welcome ' . $first_name ." ". $last_name . '!</h1><br> Today is ' . date('l, M jS, Y, H:i a.') . '<hr>';
+    
+    echo '<h3>Login Histories</h3>';  
+    echo '
+    <table class="table table-striped table-hover ">
+    <thead>
+    <tr>
+    <th>Name </th>
+    <th>Last Login</th>
+    <th>Last Logout</th>
+    </tr>
+    </thead>
+    <tbody>
+    ';
+    echo '
+    <tr>
+    <td>"'. $first_name ." ". $last_name .'"</td>
+    <td>"'.$Lastlogin.'"</td>
+    <td>"'.$Lastlogout.'"</td>
+    </tr>
+    ';
+    
+    echo '
+    </tbody>
+    </table> ';
+
+    
 }else{
     
     echo '<h1>' . $logged . '</h1><br>' . 'It is ' . date('l, M jS, Y, H:i a.');
